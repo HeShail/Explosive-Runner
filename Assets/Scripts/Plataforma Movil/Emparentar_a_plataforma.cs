ab88d7cb8fda5a8c712dfa19ba6ff24c16ce81b8
Pepe <pepe.alpha97@gmail.com>
2019-12-16 10:45:46 +0100

    Primera subida de Explosive Runner

---
+++ Assets/Scripts/Plataforma Movil/Emparentar_a_plataforma.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emparentar_a_plataforma : MonoBehaviour
+{
+
+
+    private GameObject padre;
+    public GameObject player;
+
+    private void Start()
+    {
+        padre = new GameObject();
+        //Activamos y desactivamos el GameObject siempre que lo usamos, para no crear multitud de ellos.
+        padre.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if  (other.gameObject == player)
+        {
+            padre.SetActive(true);
+            padre.transform.parent = transform;
+            player.transform.parent = padre.transform;
+            
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            player.transform.parent = null;
+            padre.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Plataforma Movil/Platform_Move.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_Move : MonoBehaviour
+{
+
+    public float distanciaX;
+    public float distanciaY;
+    public float distanciaZ;
+    private Vector3 copiaPosicion;
+    private Vector3 destino;
+    private Vector3 posicionInicial;
+    private bool isWaiting;
+    public float TIEMPO_ESPERA= 3f;
+    void Start()
+    {
+
+        destino = new Vector3(transform.position.x + distanciaX, transform.position.y + distanciaY, transform.position.z + distanciaZ);
+        posicionInicial = transform.position;
+        isWaiting = true;
+        StartCoroutine("Espera");
+    }
+
+    void Update()
+    {
+        if (isWaiting ==false) transform.position = Vector3.MoveTowards(transform.position, destino, 0.1f);
+
+        if ((transform.position == destino) && (posicionInicial != destino) && (isWaiting == false)){
+            isWaiting = true;
+            CambioSentido();
+            StartCoroutine("Espera");
+        }
+
+    }
+
+    void CambioSentido()
+    {
+        copiaPosicion = posicionInicial;
+        posicionInicial = destino;
+        destino = copiaPosicion;
+    }
+    
+    IEnumerator Espera()
+    {
+        yield return new WaitForSeconds(TIEMPO_ESPERA);
+        isWaiting = false;
+    }
+    
+}
